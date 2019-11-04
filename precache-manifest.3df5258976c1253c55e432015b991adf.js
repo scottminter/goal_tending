f734cfd792334b2cad94cf8979d8ce3037e86955
Scott Minter <sminter@gmail.com>
2019-11-04 08:30:32 -0500

    Updates

---
+++ precache-manifest.3df5258976c1253c55e432015b991adf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1f46d5fbc32f6d3a32263b19ddbce2",
+    "url": "/goal_tending/index.html"
+  },
+  {
+    "revision": "91c3d362ebe5808dd5f9",
+    "url": "/goal_tending/static/css/main.42d87482.chunk.css"
+  },
+  {
+    "revision": "6f8abd193c284260cbeb",
+    "url": "/goal_tending/static/js/2.d4037880.chunk.js"
+  },
+  {
+    "revision": "91c3d362ebe5808dd5f9",
+    "url": "/goal_tending/static/js/main.27bd4fc9.chunk.js"
+  },
+  {
+    "revision": "a7462c9a8fac2d72e93e",
+    "url": "/goal_tending/static/js/runtime~main.2570c160.js"
+  }
+]);
